998d3cda2f8161bd0b7ba78599744ebbfc4a20fe
Adriano Waltrick <adriano@unimestre.com>
2020-08-03 19:32:56 -0300

    Resolvendo o problema com aspas duplas

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
-import studyIcon from '../../assets/images/icons/study.svg';
+import studyIcon from  '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
@@ -22,20 +22,21 @@ function Landing() {
 
                 <div className="buttons-container">
                     <a href="" className="study">
-                        <img src="{studyIcon}" alt="Estudar" />
+                        <img src={studyIcon} alt="Estudar" />
                         Estudar
                     </a>
 
                     <a href="" className="study">
-                        <img src="{giveClassesIcon}" alt="Estudar" />
+                        <img src={giveClassesIcon} alt="Estudar" />
                         Estudar
                     </a>
                 </div>
 
                 <span className="total-connections">
                     Total de 200 conexões já realizadas
-                    <img src="{purpleHeartIcon}" alt="teste" />
+                    <img src={purpleHeartIcon} alt="Coreção Roxo" />
                 </span>
+
             </div>
         </div>
     )
