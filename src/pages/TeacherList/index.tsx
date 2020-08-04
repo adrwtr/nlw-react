9f1d0b043587f8ffbbb34015bf462dc6c766eae7
Adriano Waltrick <adriano@unimestre.com>
2020-08-04 20:31:38 -0300

    copy paste da parte final - ajustes de layout e tela de aulas

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
+
+import PageHeader from "../../components/PageHeader";
+
+import "./styles.css";
+import TeacherItem from "../../components/TeacherItem";
 
 function TeacherList() {
-    return (
-        <h1>Professores</h1>
-    )
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Esse são os proffys disponíveis">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" id="week_day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Horário</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem
+          avatar=""
+          name="Adriano"
+          subject="Programação"
+          subtitle="Estuda programação desde seus 12 anos."
+          description="teste descrcao"
+          price="teste"
+          phone="5511977466138"
+        />
+      </main>
+    </div>
+  );
 }
 
 export default TeacherList;
